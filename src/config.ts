86e401833dcccdc78053361e52e2c0b19d42d7d6
root <root@ip-172-31-37-139.sa-east-1.compute.internal>
2024-11-26 11:51:47 +0000

    ICBA Ver 1.0

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const MEDPLUM_BASE_URL = 'https://api.epa-bienestar.com.ar';
+export const MEDPLUM_PROJECT_ID = '088e160a-68dc-45d3-8c2d-cce41d94e296';
+export const MEDPLUM_GOOGLE_CLIENT_ID = '472653584585-gp07a070iie2o7nqan00rsf79hjavgq2.apps.googleusercontent.com';
+export const MEDPLUM_RECAPTCHA_SITE_KEY = '6LeGL4kqAAAAAE7BN1WUDokuCS26RAAvF9eevqx6';
+export const MEDPLUM_REGISTER_ENABLED = 'true';
+export const WDS_SOCKET_PORT = '4177';
